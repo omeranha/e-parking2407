82515f9560654fc66a64c71630fc17d8495adde0
omeranha <53233626+omeranha@users.noreply.github.com>
2023-08-12 16:01:16 -0300

    updating

--- eparking.py
+++ eparking.py
@@ -1,24 +1,22 @@
-from functions import *
+import time, requests, json
+from goprocam import GoProCamera
+from roboflow import Roboflow
 
-rf = Roboflow(api_key = "lQmUqij8BtbnlJ8kP0WG")
-project = rf.workspace("just-hobbies").project("hotwheel-2")
-#dataset = project.version(3).download("yolov8")
-model = project.version(3).model
-dbURL = "https://e-parking-2407-default-rtdb.firebaseio.com/"
-imagePath = "images/3carros.jpg"
-parkingSpaces = 13
-occupiedSpaces = 0
+goproCamera = GoProCamera.GoPro()
+model = Roboflow(api_key = "lQmUqij8BtbnlJ8kP0WG").workspace("just-hobbies").project("hotwheel-2").version(3).model
+imagePath = "parkinglot.jpg"
 
 while True:
-	predict = model.predict(imagePath, confidence = 50, overlap = 30) # screenshot().save("screenshot.jpg")
+	freeSpaces = 13
+	occupiedSpaces = 0
+
+	goproCamera.take_photo(0)
+	goproCamera.downloadLastMedia(custom_filename = imagePath)
+	predict = model.predict(imagePath, confidence = 60, overlap = 30)
 	for i in range(len(predict.json()["predictions"])):
 		occupiedSpaces += 1
 
-	emptySpaces = parkingSpaces - occupiedSpaces
-	requests.patch(f"{dbURL}.json", data = json.dumps({ "vagas": emptySpaces }))
-	print(f"total de vagas: {parkingSpaces} \nespaços ocupados: {occupiedSpaces} \nespaços vazios: {emptySpaces}")
-	predict.save("prediction.jpg")
-	parkingSpaces = 13
-	occupiedSpaces = 0
-	emptySpaces = parkingSpaces
+	freeSpaces = freeSpaces - occupiedSpaces
+	requests.patch("https://e-parking-2407-default-rtdb.firebaseio.com/.json", data = json.dumps({ "vagas": freeSpaces }))
+	print(f"total de vagas: 13 \nespaços ocupados: {occupiedSpaces} \nespaços vazios: {freeSpaces}")
 	time.sleep(1)
