905b4a45a3dc3616de753ace1cda2874bb96e29b
omeranha <53233626+omeranha@users.noreply.github.com>
2023-09-11 00:11:26 -0300

    removing again

--- src/render.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LoginPage from './loginPage.jsx'
-import root from './root.jsx'
-
-root.render(
-<React.StrictMode>
-	<LoginPage/>
-</React.StrictMode>
-)
--- src/root.jsx
+++
@@ -1,4 +0,0 @@
-import ReactDOM from 'react-dom/client'
-
-let root = ReactDOM.createRoot(document.getElementById('root'))
-export default root
