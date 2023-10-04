3f9902398da0048ba2f9795cfcdcfe443ed7ea76
omeranha <53233626+omeranha@users.noreply.github.com>
2023-10-04 08:39:23 -0300

    Update eparking.py

--- eparking.py
+++ eparking.py
@@ -4,7 +4,7 @@ from roboflow import Roboflow
 
 model = Roboflow(api_key = "lQmUqij8BtbnlJ8kP0WG").workspace("membrillos-etal").project("smart-vehicle-counting-and-parking-system1").version(61).model
 print("Roboflow API loaded")
-webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+webcam = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 imagePath = "parkinglot.jpg"
@@ -14,13 +14,18 @@ while True:
 	occupiedSpaces = 0
 
 	ret, frame = webcam.read()
+	print("getting webcam frame")
 	cv2.imwrite(imagePath, frame)
-	predict = model.predict(imagePath, confidence = 50, overlap = 30)
+	print("saving to file")
+	predict = model.predict(imagePath, confidence = 55, overlap = 30)
+	print("predict done")
 	for i in range(len(predict.json()["predictions"])):
 		occupiedSpaces += 1
 
 	freeSpaces = freeSpaces - occupiedSpaces
 	predict.save("predict.jpg")
+	print("image saved")
 	requests.patch("https://e-parking-2407-default-rtdb.firebaseio.com/.json", data = json.dumps({ "vagas": freeSpaces }))
+	print("patch request completed")
 	print(f"total de vagas: 13 \nespaços ocupados: {occupiedSpaces} \nespaços vazios: {freeSpaces}")
-	time.sleep(1)
+	time.sleep(.5)
